45ae0918c81edca50103ed2f82c6355293e1d124
chuhouding <chuhd@52muyou.com>
2021-11-02 21:51:35 +0800

    update

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const configPath = "config.json";
 let configOld = {};
 let configNew = {};
 let num = 0;
+let globalOldSize = 0;
+let globalNewSize = 0;
 tinify.validate(function (err) {
     if (err) {
         console.error(err);
@@ -27,12 +29,15 @@ tinify.validate(function (err) {
     textures.forEach((texture) => {
         const sizeOld = fs.statSync(texture).size;
         tinify.fromFile(texture).toFile(texture).then(() => {
+            globalOldSize += sizeOld;
             const sizeNew = fs.statSync(texture).size;
+            globalNewSize += sizeNew;
             const data = fs.readFileSync(texture);
             const md5 = crypto.createHash("md5").update(data, "utf-8").digest("hex");
             configNew[md5] = ++num;
             fs.writeFileSync(configPath, JSON.stringify(configNew, undefined, 4));
             console.log(`${++index}/${length} ${texture} ${formatBytes(sizeOld)} -> ${formatBytes(sizeNew)} ${((sizeOld - sizeNew) / sizeOld).toFixed(2)}`)
+            console.log(`${globalOldSize} -> ${globalNewSize}  ${((globalOldSize - globalNewSize) / globalOldSize).toFixed(2)}`)
         });
     })
 })
