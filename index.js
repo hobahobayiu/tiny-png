583ae04b1632d0e6737a1212e4464384eb045c59
chuhouding <chuhd@52muyou.com>
2021-11-02 21:32:12 +0800

    update

--- index.js
+++ index.js
@@ -26,12 +26,11 @@ tinify.validate(function (err) {
     console.log(length);
     textures.forEach((texture) => {
         const sizeOld = fs.statSync(texture).size;
-        const value = ++num;
         tinify.fromFile(texture).toFile(texture).then(() => {
             const sizeNew = fs.statSync(texture).size;
             const data = fs.readFileSync(texture);
             const md5 = crypto.createHash("md5").update(data, "utf-8").digest("hex");
-            configNew[md5] = value;
+            configNew[md5] = ++num;
             fs.writeFileSync(configPath, JSON.stringify(configNew, undefined, 4));
             console.log(`${++index}/${length} ${texture} ${formatBytes(sizeOld)} -> ${formatBytes(sizeNew)} ${((sizeOld - sizeNew) / sizeOld).toFixed(2)}`)
         });
